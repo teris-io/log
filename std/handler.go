0f610a11886bf19b6cddc5e8dc84ce5d3775235f
Oleg Sklyar <oleg@sklyar.io>
2017-09-21 00:02:59 +0200

    Colouring

--- std/handler.go
+++ std/handler.go
@@ -5,11 +5,11 @@ package std
 import (
 	"code.teris.io/go/log"
 	"fmt"
-	"strings"
+	"github.com/fatih/color"
 	"github.com/pkg/errors"
 	stdlog "log"
+	"strings"
 	"time"
-	"github.com/fatih/color"
 )
 
 func Use() {
@@ -21,34 +21,44 @@ type factory struct {
 }
 
 func (f *factory) With(k string, v interface{}) log.Logger {
-	return &logger{lvl: log.Unset, ctx: []kv{{k: k, v: v}}}
+	return &logger{lvl: log.Unset, fields: []Field{{Name: k, Value: v}}}
 }
 
-func SetFormatter(f func(start time.Time, level log.Level, msg string, ctx interface{}) string) {
+type Formatter func(start time.Time, level log.Level, fields []Field, msg string) string
+
+func SetFormatter(f Formatter) {
 	formatter = f
 }
 
-var formatter = func(start time.Time, lvl log.Level, msg string, ctx interface{}) string {
+var formatter = func(start time.Time, lvl log.Level, fields []Field, msg string) string {
+	timestr := color.CyanString(start.Format("02 15:04:05.000000"))
 	lvlstr := ""
 	switch lvl {
 	case log.Debug:
-		lvlstr = fmt.Sprintf(" [%s]", color.CyanString("DBG"))
+		lvlstr = fmt.Sprintf(" %s", color.New(color.Bold, color.FgYellow).Sprint("DBG"))
 	case log.Info:
-		lvlstr = fmt.Sprintf(" [%s]", color.GreenString("INF"))
+		lvlstr = fmt.Sprintf(" %s", color.New(color.Bold, color.FgGreen).Sprint("INF"))
 	case log.Error:
-		lvlstr = fmt.Sprintf(" [%s]", color.RedString("ERR"))
+		lvlstr = fmt.Sprintf(" %s", color.New(color.Bold, color.FgRed).Sprint("ERR"))
 	}
-	return fmt.Sprintf("%s%s %s %v", start.Format("02 15:04:05.999999"), lvlstr, msg, ctx)
+	fieldstr := ""
+	for _, f := range fields {
+		if fieldstr != "" {
+			fieldstr += ", "
+		}
+		fieldstr += fmt.Sprintf("{%s: %v}", color.New(color.Bold).Sprint(f.Name), f.Value)
+	}
+	return fmt.Sprintf("%s%s %s %s", timestr, lvlstr, msg, fieldstr)
 }
 
 type logger struct {
-	lvl log.Level
-	ctx []kv
+	lvl    log.Level
+	fields []Field
 }
 
-type kv struct {
-	k string
-	v interface{}
+type Field struct {
+	Name  string
+	Value interface{}
 }
 
 type stackTracer interface {
@@ -56,15 +66,15 @@ type stackTracer interface {
 }
 
 func (l *logger) With(k string, v interface{}) log.Logger {
-	return &logger{lvl: l.lvl, ctx: append([]kv{{k: k, v: v}}, l.ctx...)}
+	return &logger{lvl: l.lvl, fields: append([]Field{{Name: k, Value: v}}, l.fields...)}
 }
 
 func (l *logger) WithLevel(lvl log.Level) log.Logger {
-	return &logger{lvl: lvl, ctx: append([]kv{}, l.ctx...)}
+	return &logger{lvl: lvl, fields: append([]Field{}, l.fields...)}
 }
 
 func (l *logger) WithError(err error) log.Logger {
-	ctx := []kv{{k: "error", v: err.Error()}}
+	ctx := []Field{{Name: "error", Value: err.Error()}}
 
 	if s, ok := err.(stackTracer); ok {
 		frame := s.StackTrace()[0]
@@ -78,15 +88,15 @@ func (l *logger) WithError(err error) log.Logger {
 			file = parts[1]
 		}
 
-		ctx = append(ctx, kv{k: "source", v: fmt.Sprintf("%s(%s:%s)", name, file, line)})
+		ctx = append(ctx, Field{Name: "source", Value: fmt.Sprintf("%s(%s:%s)", name, file, line)})
 	}
-	return &logger{lvl: log.Error, ctx: append(ctx, l.ctx...)}
+	return &logger{lvl: log.Error, fields: append(ctx, l.fields...)}
 }
 
 func (l *logger) Log(msg string) log.Tracer {
 	start := time.Now()
-	stdlog.Println(formatter(start, l.lvl, msg, l.ctx))
-	return log.NewTracer(&logger{lvl: l.lvl, ctx: append([]kv{}, l.ctx...)}, start)
+	stdlog.Println(formatter(start, l.lvl, l.fields, msg))
+	return log.NewTracer(&logger{lvl: l.lvl, fields: append([]Field{}, l.fields...)}, start)
 }
 
 func (l *logger) Logf(format string, v ...interface{}) log.Tracer {
--- std/handler_test.go
+++ std/handler_test.go
@@ -4,38 +4,26 @@ package std_test
 
 import (
 	"code.teris.io/go/log"
-	"testing"
-	"time"
 	"code.teris.io/go/log/std"
 	"fmt"
+	"github.com/pkg/errors"
 	stdlog "log"
 	"os"
-	"github.com/pkg/errors"
-	"github.com/fatih/color"
+	"testing"
+	"time"
 )
 
 func init() {
 	std.Use()
-	std.SetFormatter(func(start time.Time, lvl log.Level, msg string, ctx interface{}) string {
-		lvlstr := ""
-		switch lvl {
-		case log.Debug:
-			lvlstr = fmt.Sprintf(" [%s]", color.CyanString("DBG"))
-		case log.Info:
-			lvlstr = fmt.Sprintf(" [%s]", color.GreenString("INF"))
-		case log.Error:
-			lvlstr = fmt.Sprintf(" [%s]", color.RedString("ERR"))
-		}
-		return fmt.Sprintf("%s%s %s %v", start.Format("02/01 15:04:05.999"), lvlstr, msg, ctx)
-	})
 	stdlog.SetOutput(os.Stdout)
 }
 
 func TestLogger_Log(t *testing.T) {
-	tracer := log.With("context", "Foo").Log("something happened")
-	time.Sleep(time.Second)
-	tracer.Trace()
-	time.Sleep(time.Second)
+	logger := log.With("context", "Foo")
+	logger.Log("something happened")
+	logger.WithLevel(log.Debug).Log("some degub message")
+	logger.WithLevel(log.Info).Log("some info message")
+	logger.WithLevel(log.Error).Log("some error message")
 }
 
 func TestLogger_LogError(t *testing.T) {
